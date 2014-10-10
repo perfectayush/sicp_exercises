43e056b98b804718551ff39d4cf233e9fa9f8847
Ayush Goyal <ayush.go@directi.com>
2014-10-10 22:37:22 +0530

    update ex 2.28

--- Chapter-2/exercise 2-28 fringe.rkt
+++ Chapter-2/exercise 2-28 fringe.rkt
@@ -13,8 +13,20 @@
           (else                   (fringe-r (cdr x) (myappend y (fringe-r (car x) nil))))))
   (fringe-r x nil))
 
+(define (fringe2 x)
+  (cond 
+   ((null? x) nil)
+   ((not (pair? x)) (list x))
+   (else (myappend (fringe2 (car x)) (fringe2 (cdr x))))))
+
 (define y (list (list 1 2) (list 3 4)))
 (define z (list y y))
 
 (display (fringe y))
+(display "\n")
 (display (fringe z))
+(display "\n")
+(display (fringe2 y))
+(display "\n")
+(display (fringe2 z))
+(display "\n")
